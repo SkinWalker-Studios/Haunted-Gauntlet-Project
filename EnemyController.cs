bffb9a6867952dd8fa2d57eb46f5280b7275dcaf
Peter8a473 <112195599+Peter8a473@users.noreply.github.com>
2023-01-28 16:10:42 -0500

    Add files via upload

---
+++ EnemyController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // enemy stats
+    public float speed = 1.0f;
+    public int maxHealth = 100;
+    int currentHealth;
+
+    PlayerController player = other.gameobject.GetComponent<PlayerController>;
+
+    Rigidbody2D rigidbody2D;
+ 
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2D.position;
+
+        // finds the player's position
+        player.posX = posX;
+        player.posY = posY;
+
+        // calculates the direction towards the player
+        float distanceX = |position.x - posX|;
+        float distanceY = |position.y - posY|;
+        float distance = sqrt(pow(distanceX, 2) + pow(distanceY, 2));
+        float directionX = distanceX / distance;
+        float directionY = distanceY / distance;
+
+        // changes the position
+        position.y += Time.deltaTime * speed * directionX;
+        position.x += Time.deltaTime * speed * directionY;
+
+        // creates a new vector
+        Vector2 move = new Vector2(directionX, directionY);
+
+        // sets the direction
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            moveDirection.Set(move.x, move.y);
+            moveDirection.Normalize();
+        }
+
+        // adds animation to the direction
+        animator.SetFloat("Move X", moveDirection.x);
+        animator.SetFloat("Move Y", moveDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        // updates position
+        rigidbody2D.MovePosition(position);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // damage from projectile
+        if (other.gameObject.CompareTag("")) // SET TAG
+        {
+            ChangeHealth(0); // SET VALUE
+            Destroy(other.gameObject);
+        }
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth += amount;
+
+        if (currentHealth <= 0)
+        {
+            Destroy(GameObject)
+            player.ChangeScore(0) // SET VALUE
+        }
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,181 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // player stats
+    public float speed = 1.0f;
+    public int maxHealth = 2000;
+    int currentHealth;
+    int currentScore;
+
+    // collectables
+    public int keyAmount;
+    public int potionAmount;
+
+    // tracking player movement & position
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    public float posX;
+    public float posY;
+
+    // audio
+    public AudioSource musicsource;
+    AudioSource audioSource;
+      // INSERT CLIPS
+
+    // world variables
+    bool gameOver = false;
+
+    // animation
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+    
+    public GameObject projectilePrefab; // SET PROJECTILE
+
+    void Start()
+    {
+        // set base variables
+        currentHealth = maxHealth;
+        currentScore = 0;
+        keyAmount = 0;
+        potionAmount = 0;
+        posX = 0; // SET VALUE
+        posY = 0; // SET VALUE
+
+        // background music
+        audioSource = GetComponent<AudioSource>();
+        musicSource.clip = backgroundMusic; // SET BACKGROUND MUSIC
+        musicSource.Play();
+    }
+
+    void Update()
+    {
+        // movement inputs
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        // animation states
+        Vector2 move = new Vector2(horizontal, vertical);
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        // firing projectiles
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Launch();
+        }
+
+        // esc to exit
+        if (Input.GetKey(KeyCode.ESC))
+        {
+            // FILL IN CONDITIONAL
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // movements
+        Vector2 position = rigidbody2d.position;
+        position.x += speed * horizontal * Time.deltaTime;
+        position.y += speed * vertical * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+        posX = position.x;
+        posY = position.y;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // key collectable
+        if (other.gameObject.CompareTag("")) // SET TAG
+        {
+            ChangeKeys(1);
+            Destroy(other.gameObject);
+        }
+
+        // health collectable
+        if (other.gameObject.CompareTag("")) // SET TAG
+        {
+            ChangeHealth(0); // SET VALUE
+            Destroy(other.gameObject);
+        }
+
+        // potion collectable
+        if (other.gameObject.CompareTag("")) // SET TAG
+        {
+            ChangePotions(1);
+            Destroy(other.gameObject);
+        }
+
+        // enemy contact
+        if (other.gameObject.CompareTag("")) // SET TAG
+        {
+            ChangeHealth(0); // SET VALUE
+            Destroy(other.gameObject);
+        }
+
+        // treasure collectable
+        if (other.gameObject.CompareTag("")) // SET TAG
+        {
+            ChangeScore(0); // SET VALUE
+            Destroy(other.gameObject);
+        }
+    }
+
+    void ChangeHealth(int amount)
+    {
+        again = true;
+
+        currentHealth += amount;
+
+        if (currentHealth <= 0 && again == true)
+        {
+            loseText.SetActive(true);
+            speed = 0;
+            gameOver = true;
+            again = false;
+        }
+    }
+
+    void ChangeScore(int amount)
+    {
+        currentScore += amount;
+    }
+
+    void ChangeKeys(int amount)
+    {
+        keyAmount += amount;
+    }
+
+    void ChangePotion(int amount)
+    {
+        potionAmount += amount;
+    }
+
+    // fires a projectile
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+
+        PlaySound(throwSound); // SET LAUNCH CLIP
+    }
+
+    // plays a clip
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+}
