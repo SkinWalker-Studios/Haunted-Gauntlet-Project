76ea047a0f609e3623e2e939b5d6923551012093
Peter8a473 <112195599+Peter8a473@users.noreply.github.com>
2023-02-01 17:22:55 -0500

    Update EnemyController.cs

--- EnemyController.cs
+++ EnemyController.cs
@@ -1,52 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class EnemyController : MonoBehaviour
 {
     // enemy stats
-    public float speed = 1.0f;
+    public float speed = 3.0f;
     public int maxHealth = 100;
     int currentHealth;
 
-    PlayerController player = other.gameobject.GetComponent<PlayerController>;
-
     Rigidbody2D rigidbody2D;
- 
-    Animator animator;
+
+    // Animator animator;
+    Vector2 moveDirection = new Vector2(1, 0);
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
         rigidbody2D = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+       // animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
+        
+            
         Vector2 position = rigidbody2D.position;
 
         // finds the player's position
-        float posX = player.posX;
-        float posY = player.posY;
+        float positX;
+        positX = FindObjectOfType<PlayerController>().posX;
+        float positY;
+        positY = FindObjectOfType<PlayerController>().posX;
 
         // calculates the direction towards the player
-        float distanceX = position.x - posX;
-        float distanceY = position.y - posY;
-        float distance = Mathf.Sqrt(Mathf.Pow(distanceX, 2) + pow(distanceY, 2));
+        float distanceX = -position.x + positX;
+        float distanceY = -position.y + positY;
+        float distance = Mathf.Sqrt(Mathf.Pow(distanceX, 2) + Mathf.Pow(distanceY, 2));
         float directionX = distanceX / distance;
         float directionY = distanceY / distance;
 
         // changes the position
-        position.y += Time.deltaTime * speed * directionX;
-        position.x += Time.deltaTime * speed * directionY;
+        position.y += Time.deltaTime * speed * directionY;
+        position.x += Time.deltaTime * speed * directionX;
 
         // creates a new vector
         Vector2 move = new Vector2(directionX, directionY);
 
         // sets the direction
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        /*if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
             moveDirection.Set(move.x, move.y);
             moveDirection.Normalize();
@@ -56,11 +60,11 @@ public class EnemyController : MonoBehaviour
         animator.SetFloat("Move X", moveDirection.x);
         animator.SetFloat("Move Y", moveDirection.y);
         animator.SetFloat("Speed", move.magnitude);
-
+        */
         // updates position
         rigidbody2D.MovePosition(position);
     }
-    
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Projectile")) // SET TAG
@@ -69,7 +73,7 @@ public class EnemyController : MonoBehaviour
             Destroy(other.gameObject);
         }
     }
-        
+
     void OnTriggerStay2D(Collider2D other)
     {
         // player contact
@@ -85,8 +89,8 @@ public class EnemyController : MonoBehaviour
 
         if (currentHealth <= 0)
         {
-            Destroy(GameObject);
-            player.ChangeScore(100); // SET VALUE
+            // Destroy(GameObject);
+            // FindObjectOfType<PlayerController>().ChangeScore(100); // SET VALUE
         }
     }
 }
